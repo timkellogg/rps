f3e39327eaeef8bc5f91ffc95a0a4798a2da7cb2
Julianna Green & Tim Kellogg <student@epicodus.com>
2015-08-05 15:15:42 -0700

    finish core method logic of rock paper scissors

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -8,6 +8,26 @@ class String
     if player == "rock"
       if opponent == "scissors"
         true
+      elsif opponent == "paper"
+        false
+      elsif opponent == self
+        "tie"
+      end
+    elsif player == "scissors"
+      if opponent == "rock"
+        false
+      elsif opponent == "paper"
+        true
+      elsif opponent == "scissors"
+        "tie"
+      end
+    elsif player == "paper"
+      if opponent == "rock"
+        true
+      elsif opponent == "scissors"
+        false
+      elsif opponent == "paper"
+        "tie"
       end
     end
   end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -6,4 +6,13 @@ describe('String#rock_paper_scissors') do
   it("returns true if self beats argument") do
     expect("rock".rock_paper_scissors("scissors")).to(eq(true))
   end
+
+  it("returns false if self does not beat argument") do
+    expect("rock".rock_paper_scissors("paper")).to(eq(false))
+  end
+
+  it("returns tie if self is equal to the argument") do
+    expect("rock".rock_paper_scissors("rock")).to(eq("tie"))
+  end
+
 end
